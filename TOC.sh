c75f628e171a5159cf3bd1b40a6b92b0dc9f9ae8
alexh-name <1@alexh.name>
2016-06-29 22:06:36 +0200

    TOC.sh: missing a end tag

--- TOC.sh
+++ TOC.sh
@@ -3,5 +3,5 @@
 file="$1"
 
 printf '%s\n' "<ul>"
-grep '<h2\ id=".*">.*</h2>' ${file} | sed -e 's/<h2\ id=\"/<li><a\ href=\"#/g' -e 's/<\/h2>/<\/li>/g'
+grep '<h2\ id=".*">.*</h2>' ${file} | sed -e 's/<h2\ id=\"/<li><a\ href=\"#/g' -e 's/<\/h2>/<\/a><\/li>/g'
 printf '%s\n' "</ul>"
